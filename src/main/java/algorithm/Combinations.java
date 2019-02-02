107e729b0ac7a3bff3cec1ef1e2de05a5b432183
yocn <zhaoyingkun@163.com>
2019-02-02 14:30:22 +0800

    update

---
+++ src/main/java/algorithm/Combinations.java
@@ -0,0 +1,38 @@
+package algorithm;
+
+import java.util.List;
+
+/**
+ * Created by yocn on 2019/2/1.
+ * Given two integers n and k, return all possible combinations of k numbers out of 1 ... n.
+ * 给定两个整数 n 和 k，返回 1 ... n 中所有可能的 k 个数的组合。
+ * <p>
+ * Example:
+ * <p>
+ * Input: n = 4, k = 2
+ * Output:
+ * [
+ * [2,4],
+ * [3,4],
+ * [2,3],
+ * [1,2],
+ * [1,3],
+ * [1,4],
+ * ]
+ */
+public class Combinations {
+    /**
+     * 床头两条TYPE-C充电线带头 一根USB-B线充充电宝的
+     * 我的两件衣服
+     * 洗漱用品化妆品
+     * 我的刮胡刀
+     * 你的身份证
+     *
+     * @param n
+     * @param k
+     * @return
+     */
+    public List<List<Integer>> combine(int n, int k) {
+
+    }
+}
